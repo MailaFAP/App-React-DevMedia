3cb97367c4bbd966f59718a5e96b5c09e1c9495c
MailaFAP <maila_3@hotmail.com>
2023-09-15 13:07:24 -0300

    adicionei componentes

---
+++ src/Componentes/HomePage/home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Card from '../Card/card';
+import './homePage.css';
+
+export default function HomePage() {
+  return (
+    <div className="home-page">
+      <Card />
+      <Card />
+      <Card />
+    </div>
+  );
+}
